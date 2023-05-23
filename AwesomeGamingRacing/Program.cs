def77c6b780e9b3ddbdbc166ecf049e92f70e8fd
BrandinGoldsberry <bgoldsberry@student.neumont.edu>
2023-05-22 19:07:13 -0700

    Removed test route

--- AwesomeGamingRacing/Program.cs
+++ AwesomeGamingRacing/Program.cs
@@ -32,8 +32,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapGet("/", () => "Hello ForwardedHeadersOptions!");
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
